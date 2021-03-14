65d56f3b846fe01dffc1e6186d7f282521aa3920
Talita-8 <tlthmf1@outlook.com>
2021-03-14 19:52:49 -0300

    Refactoring about page

--- src/pages/about/components.js
+++ src/pages/about/components.js
@@ -1,3 +1,24 @@
 export const aboutFrame = `
-<p>REFATORANDO...</p>
-`
+<div class="about-container">
+  <picture>
+    <img class="img-imperator" src="./images/imperador.jpg" alt="Julio Cesar Imperador">
+  </picture>
+
+  <article class="about-text">
+    <h1>História</h1>
+    <hr>
+    <p>
+      A cifra de César foi nomeada em homenagem a Júlio César que, segundo Suetónio, a usava com uma troca de três 
+      posições para proteger mensagens de significado militar. 
+      É desconhecido o quão efetiva era a cifra de César nesta época, mas é provável que fosse razoavelmente 
+      segura, ainda mais porque a maioria dos inimigos de César eram analfabetos e outros presumiam que as mensagens
+      estavam escritas em uma língua estrangeira desconhecida.
+    </p>
+
+    <picture>
+     <img class="img-medallion" src="./images/medalhão.png" alt="Medalhão Cifra de César">
+    </picture>
+
+  </article>
+</div>
+`;
